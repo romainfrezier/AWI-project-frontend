7f6741357b50561e4a85f87764a58fb295450155
romainfrezier <romainfrezier@gmail.com>
2023-02-02 22:23:09 +0100

    Add landing page, error 404 page and logo on header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./core/components/home/home.component";
+import {ErrorComponent} from "./core/components/error/error.component";
 
 const routes: Routes = [
   { path: 'volunteers', loadChildren: () => import('./volunteers/volunteers.module').then(m => m.VolunteersModule) },
   { path: 'games', loadChildren: () => import('./games/games.module').then(m => m.GamesModule) },
   { path: 'assignments', loadChildren: () => import('./assignments/assignments.module').then(m => m.AssignmentsModule) },
-  { path: '**', redirectTo: 'applications'}
+  { path: '', component: HomeComponent },
+  { path: 'error404', component: ErrorComponent},
+  { path: '**', redirectTo: 'error404'}
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,12 +5,16 @@ import {SharedModule} from "../shared/shared.module";
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     CommonModule,
